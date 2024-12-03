07e25383c8d0dd2b93c74d0f75323e59ba727d8a
SwadhinDwibedy <swsdhindwibedy@gmail.com>
2024-12-03 22:54:43 +0530

    add this practice

---
+++ app/src/main/java/com/example/countermvvm/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.countermvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.countermvvm.ui.theme.CounterMVVmTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val viewModel: CounterViewModel = viewModel()
+            CounterMVVmTheme {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                    ){
+                        theCounterApp(viewModel)
+                    }
+            }
+        }
+    }
+}
+@Composable
+fun theCounterApp(viewModel: CounterViewModel) {
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Count: ${viewModel.count.value}",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row (horizontalArrangement = Arrangement.spacedBy(8.dp)){
+            Button(onClick = { viewModel.increment() }) {
+                Text("Increment")
+            }
+            Button(onClick = {viewModel. decrement() }) {
+                Text("Decrement")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/countermvvm/ui/theme/CounterModel.kt
@@ -0,0 +1,18 @@
+package com.example.countermvvm.ui.theme
+
+data class CounterModel (var count : Int)
+ class CounterRepository(){
+     private val _counter = CounterModel(0)
+      fun getCounter() = _counter
+
+     fun incrementCounter(){
+         _counter.count++
+     }
+
+     fun decrementCounter(){
+         _counter.count--
+     }
+
+
+
+ }
